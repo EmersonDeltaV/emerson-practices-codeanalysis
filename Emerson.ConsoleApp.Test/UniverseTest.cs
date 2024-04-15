b58049c22464692882f3d72bb4eef0ff7f306f10
EMRSN\E1476737 <vincentjames.paz@emerson.com>
2024-04-15 10:37:44 +0800

    additional unit test for Universe and code analysis issues

--- Emerson.ConsoleApp.Test/UniverseTest.cs
+++ Emerson.ConsoleApp.Test/UniverseTest.cs
@@ -1,4 +1,7 @@
-﻿namespace Emerson.ConsoleApp.Test
+﻿using System;
+using System.Collections;
+
+namespace Emerson.ConsoleApp.Test
 {
     public class UniverseTest
     {
@@ -9,7 +12,18 @@
 
             var target = new Universe(new List<Entity>() { planet1 });
 
-            Assert.Equal(1, target.Entities.Count());
+            Assert.Equal(1, target.Entities.Count);
+        }
+
+        [Fact]
+        public void Add_2Planet_ShouldContain_2Entity()
+        {
+            var planet1 = new Planet("Planet1");
+            var planet2 = new Planet("Planet2");
+
+            var target = new Universe(new List<Entity>() { planet1, planet2 });
+
+            Assert.Equal(2, target.Entities.Count);
         }
     }
 }
